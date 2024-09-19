816eff733d04833017589a226adb4f54bd952262
djena <eng.djena075@gmail.com>
2024-09-19 22:26:44 +0530

    Update easy.py, without using os module

--- beginner_module_OS/easy.py
+++ beginner_module_OS/easy.py
@@ -6,4 +6,13 @@ the current directory
 import os
 
 if __name__ == '__main__':
-    print(f"current directory is {os.getcwd()}")
+    print(f"Current directory is {os.getcwd()}")
+
+# Without importing os module
+import subprocess
+
+def get_current_dir():
+    cur_dir = subprocess.run("pwd", capture_output=True, text=True)
+    return cur_dir.stdout.strip()
+
+print(f"Current Directory is {get_current_dir()}")
