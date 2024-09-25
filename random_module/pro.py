6efd92968266a054f393774f4fe452cd53feeaa9
djena <eng.djena075@gmail.com>
2024-09-25 22:22:34 +0530

    New pro problem to 6 random number between certain range

---
+++ random_module/pro.py
@@ -0,0 +1,20 @@
+'''
+Implement a program that simulates a lottery draw, 
+where 6 unique random numbers are selected from a range of 1 to 49.
+'''
+
+from random import randint
+
+def lottery_draw():
+    # Define the range of numbers
+    min_num = 1
+    max_num = 49
+    num_to_select = 6
+    numbers = set()
+    while len(numbers) < num_to_select:
+        numbers.add(randint(min_num, max_num))
+        
+    print(list(numbers))
+
+if __name__ == '__main__':
+    lottery_draw()
