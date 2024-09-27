86bf5ba25ff43ac5bf70613e2f0d2ad7e23dd42d
djena <eng.djena075@gmail.com>
2024-09-27 21:37:41 +0530

    New challenge proble to calculate UTC timezone time

--- date_time_module/advance.py
+++ date_time_module/advance.py
@@ -9,7 +9,7 @@ def utc_timezone():
     current_loc_time = datetime.now().time()
     utc_time = datetime.now(timezone.utc).time()
     print(f"Current time in your location: {current_loc_time}")
-    print(f"Current time in UTC: {utc_time}")
+    print(f"Current time in UTC timezone: {utc_time}")
 
 if __name__ == '__main__':
     utc_timezone()
